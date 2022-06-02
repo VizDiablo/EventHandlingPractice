6a698d8c32927edc22912fe37ead4c43ab6ec5d8
VizDiablo <79822447+VizDiablo@users.noreply.github.com>
2022-06-02 10:55:07 -0600

    After lesson commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [headingText, setHeadingText] = useState("");
+  const [isMousedOver, setMousedOver] = useState(false);
+
+  function handleClick() {
+    setHeadingText("Submitted");
+  }
+
+  function handleMouserOver() {
+    setMousedOver(true);
+  }
+
+  function handleMouseLeave() {
+    setMousedOver(false);
+  }
+
+  function handleInputChanged(event) {
+    console.log("Text changed");
+    console.log(event.target.value);
+    setHeadingText(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {headingText}</h1>
+      <input
+        onChange={handleInputChanged}
+        type="text"
+        placeholder="What's your name?"
+        value={headingText}
+      />
+      <button
+        style={{ backgroundColor: isMousedOver ? "black" : "white" }}
+        onClick={handleClick}
+        onMouseOver={handleMouserOver}
+        //onMouseLeave={handleMouseLeave}
+        onMouseOut={handleMouseLeave}
+      >
+        Submit
+      </button>
     </div>
   );
 }
